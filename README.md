44bf480550bcdd097b221f6cfb33e95004ea4239
Toshi7 <ttoshi0719@gmail.com>
2019-03-20 21:59:09 +0900

    add lux sensor

---
+++ README.md
@@ -0,0 +1,12 @@
+# Waterproof Temperature Sensor & Luminosity-sensor
+
+## Purpose
+
+## sensor
+  - 18B20 Waterproof Digital Temperature Temp Sensor
+  - TSL 2561 Lux sensor
+
+## Installing platformio & library for SparkFunTSL2561 which is necessary to use TSL2561 and OneWire for 18B20 Waterproof Digital Temperature Temp Sensor
+
+[circuit](http://make.bcde.jp/arduino/ds18b201-wire%E3%81%A7%E6%B8%A9%E5%BA%A6%E8%A8%88%E6%B8%AC/)
+[reference](http://www.esp32learning.com/code/esp32-and-a-tsl2561-luminosity-sensor-example.php)
--- esp32/src/main.ino
+++ esp32/src/main.ino
@@ -1,20 +1,166 @@
+#include <SparkFunTSL2561.h>
+#include <Wire.h>
 #include <OneWire.h>
  
 // DS18S20 Temperature chip i/o
-OneWire ds(23);  // on pin 6
+OneWire ds(19);  // on pin 6
+
+// Create an SFE_TSL2561 object, here called "light":
+ 
+SFE_TSL2561 light;
+ 
+// Global variables:
+ 
+boolean gain; // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms; // Integration ("shutter") time in milliseconds
  
 void setup(void) {
   // initialize inputs/outputs
   // start serial port
   Serial.begin(115200);
+  Serial.println("Temp & TSL2561 example sketch");
+  // Initialize the SFE_TSL2561 library
+ 
+light.begin();
+ 
+// Get factory ID from sensor:
+// (Just for fun, you don't need to do this to operate the sensor)
+ 
+unsigned char ID;
+ 
+if (light.getID(ID))
+{
+Serial.print("Got factory ID: 0X");
+Serial.print(ID,HEX);
+Serial.println(", should be 0X5X");
+}
+// Most library commands will return true if communications was successful,
+// and false if there was a problem. You can ignore this returned value,
+// or check whether a command worked correctly and retrieve an error code:
+else
+{
+byte error = light.getError();
+printError(error);
+}
+ 
+// The light sensor has a default integration time of 402ms,
+// and a default gain of low (1X).
+ 
+// If you would like to change either of these, you can
+// do so using the setTiming() command.
+ 
+// If gain = false (0), device is set to low gain (1X)
+// If gain = high (1), device is set to high gain (16X)
+ 
+gain = 0;
+ 
+// If time = 0, integration will be 13.7ms
+// If time = 1, integration will be 101ms
+// If time = 2, integration will be 402ms
+// If time = 3, use manual start / stop to perform your own integration
+ 
+unsigned char time = 2;
+ 
+// setTiming() will set the third parameter (ms) to the
+// requested integration time in ms (this will be useful later):
+ 
+Serial.println("Set timing...");
+light.setTiming(gain,time,ms);
+ 
+// To start taking measurements, power up the sensor:
+ 
+Serial.println("Powerup...");
+light.setPowerUp();
+ 
+// The sensor will now gather light during the integration time.
+// After the specified time, you can retrieve the result from the sensor.
+// Once a measurement occurs, another integration period will start.
 }
  
 void loop(void) {
+  // ms = 1000;
+  // light.manualStart();
+  delay(ms);
+
+  // light.manualStop();
+ 
+// Once integration is complete, we'll retrieve the data.
+ 
+// There are two light sensors on the device, one for visible light
+// and one for infrared. Both sensors are needed for lux calculations.
+ 
+// Retrieve the data from the device:
+ 
+unsigned int data0, data1;
+ 
+if (light.getData(data0,data1))
+{
+// getData() returned true, communication was successful
+ 
+Serial.print("data0: ");
+Serial.print(data0);
+Serial.print(" data1: ");
+Serial.print(data1);
+
+// To calculate lux, pass all your settings and readings
+// to the getLux() function.
+ 
+double lux; // Resulting lux value
+boolean good; // True if neither sensor is saturated
+ 
+// Perform lux calculation:
+ 
+good = light.getLux(gain,ms,data0,data1,lux);
+ 
+// Print out the results:
+ 
+Serial.print(" lux: ");
+Serial.print(lux);
+if (good) Serial.println(" (good)"); else Serial.println(" (BAD)");
+}
+else
+{
+// getData() returned false because of an I2C error, inform the user.
+ 
+byte error = light.getError();
+printError(error);
+}
+
   float temperature = getTemperature();
  
   Serial.print(temperature);
   Serial.println();
   delay(2000);
+}
+
+void printError(byte error)
+// If there's an I2C error, this function will
+// print out an explanation.
+{
+Serial.print("I2C error: ");
+Serial.print(error,DEC);
+Serial.print(", ");
+ 
+switch(error)
+{
+case 0:
+Serial.println("success");
+break;
+case 1:
+Serial.println("data too long for transmit buffer");
+break;
+case 2:
+Serial.println("received NACK on address (disconnected?)");
+break;
+case 3:
+Serial.println("received NACK on data");
+break;
+case 4:
+Serial.println("other error");
+break;
+default:
+Serial.println("unknown error");
+}
 }
  
 void updateData(byte* data) {
